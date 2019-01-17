2c46fbac3e1c39dafb4965cc42603fdaf8aec96d
Roman-Liubinets <roman.liubinets@gmail.com>
2019-01-17 15:13:09 +0200

    some stuff with login styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -9,6 +10,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +19,9 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     MaterialModule,
     AuthModule,
+    AppRoutingModule,
     // EffectsModule.forRoot([]),
+    TooltipModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthComponent } from './auth.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, AuthRoutingModule],
   declarations: [LoginComponent, RegisterComponent, AuthComponent],
 })
 export class AuthModule {}
