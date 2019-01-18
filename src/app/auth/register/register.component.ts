a945dd7f702dd9c62848c5e7e465656da712ec08
Roman-Liubinets <roman.liubinets@gmail.com>
2019-01-18 12:17:21 +0200

    HTML, SCSS, Reactive Form, Validation in Register Component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.formInit();
   }
 
+  formInit() {
+    this.form = this.fb.group({
+      Email: ['', [Validators.required, Validators.email]],
+      Password: ['', [Validators.required, Validators.minLength(1)]],
+      Check_Pass: ['', [Validators.required, Validators.minLength(1)]],
+    });
+  }
+
+  private checkValidation(formGroup: FormGroup) {
+    (<any>Object).values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+    });
+  }
+
+  onRegister() {
+    this.checkValidation(this.form);
+    if (this.form.valid) {
+      console.log('true');
+    }
+  }
 }
