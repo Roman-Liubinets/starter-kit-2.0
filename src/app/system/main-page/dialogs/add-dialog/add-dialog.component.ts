c9311da215c9eb1a9cbb5079629da5045be2e533
Roman-Liubinets <roman.liubinets@gmail.com>
2019-01-30 16:45:11 +0200

    import Reactive Forms Module in Main Page module

--- src/app/system/main-page/dialogs/add-dialog/add-dialog.component.ts
+++ src/app/system/main-page/dialogs/add-dialog/add-dialog.component.ts
@@ -1,15 +1,33 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-add-dialog',
   templateUrl: './add-dialog.component.html',
   styleUrls: ['./add-dialog.component.scss'],
 })
-export class AddDialogComponent {
+export class AddDialogComponent implements OnInit {
+  form: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<AddDialogComponent>,
   ) {}
+
+  ngOnInit() {
+    this.formInit();
+  }
+
+  formInit() {
+    this.form = this.fb.group({
+      first: ['', [Validators.required, Validators.minLength(1)]],
+      last: ['', [Validators.required, Validators.minLength(1)]],
+      something: ['', [Validators.required, Validators.minLength(1)]],
+    });
+  }
+
+  save() {}
 
   closeDialog() {
     this.dialogRef.close();
--- src/app/system/main-page/main-page.module.ts
+++ src/app/system/main-page/main-page.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MaterialModule } from '../../material.module';
-// import { SystemRoutingModule } from './system-routing.module';
-// import { SystemComponent } from './system.component';
 import { MainPageComponent } from '../main-page/main-page.component';
 import { AddDialogComponent } from '../main-page/dialogs/add-dialog/add-dialog.component';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
   declarations: [MainPageComponent, AddDialogComponent],
   entryComponents: [AddDialogComponent],
 })
