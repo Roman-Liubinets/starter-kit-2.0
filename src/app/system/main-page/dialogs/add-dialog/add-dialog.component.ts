a7ba686683db3440aaf3697b82ffc4bf0bc6d01b
Roman-Liubinets <roman.liubinets@gmail.com>
2019-01-30 16:15:41 +0200

    Material Dialog now open correct

--- src/app/system/main-page/dialogs/add-dialog/add-dialog.component.ts
+++ src/app/system/main-page/dialogs/add-dialog/add-dialog.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialog } from '@angular/material';
 @Component({
   selector: 'app-add-dialog',
   templateUrl: './add-dialog.component.html',
-  styleUrls: ['./add-dialog.component.scss']
+  styleUrls: ['./add-dialog.component.scss'],
 })
 export class AddDialogComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
