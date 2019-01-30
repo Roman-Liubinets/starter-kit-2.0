bdc9cea2a46cd1797cc076eba967a7b6c9d06f85
Roman-Liubinets <roman.liubinets@gmail.com>
2019-01-30 11:50:29 +0200

    if Login data not right shows alert Messages

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,6 +7,7 @@ import * as fromAuth from '../store';
 
 import { AuthService } from '../services/auth.service';
 import { Login } from '../models/auth.model';
+import { Message } from '../models/message.model';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +16,7 @@ import { Login } from '../models/auth.model';
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
+  message: Message;
 
   constructor(
     private fb: FormBuilder,
@@ -23,7 +25,14 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.message = new Message('danger', '');
     this.formInit();
+  }
+  private showMessage(text: string, type: string = 'danger') {
+    this.message = new Message(type, text);
+    window.setTimeout(() => {
+      this.message.text = '';
+    }, 3000);
   }
 
   formInit() {
@@ -43,25 +52,26 @@ export class LoginComponent implements OnInit {
     this.checkValidation(this.form);
     if (this.form.valid) {
       this.store.dispatch(new fromAuth.Login(this.form.get('Email').value));
-      this.store.select(fromAuth.getLoaded).subscribe((loaded: boolean) => {
-        console.log('TCL: LoginComponent -> onLogin -> loaded', loaded);
-        if (loaded) {
-          this.store
-            .select(fromAuth.getAuthDataState)
-            .subscribe((store_data: any) => {
-              if (store_data.login_response_data) {
-                if (
-                  store_data.login_response_data.Email ===
-                  this.form.get('Email').value
-                ) {
-                  console.log('Logged!!!');
-                }
+      this.store
+        .select(fromAuth.getAuthDataState)
+        .subscribe((store_data: any) => {
+          if (store_data.login_response_data) {
+            if (
+              store_data.login_response_data.Email ===
+              this.form.get('Email').value
+            ) {
+              if (
+                store_data.login_response_data.Password ===
+                this.form.get('Password').value
+              ) {
               } else {
-                return;
+                this.showMessage('Password incorrect!', 'danger');
               }
-            });
-        }
-      });
+            }
+          } else {
+            this.showMessage('This user does not exist !', 'danger');
+          }
+        });
     }
   }
 }
