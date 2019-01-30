2f2b3c25fe05d138b878c2ff8d56790066d2129e
Roman-Liubinets <roman.liubinets@gmail.com>
2019-01-30 15:23:19 +0200

    generet header of system

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SystemRoutingModule } from './system-routing.module';
 import { SystemComponent } from './system.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [SystemRoutingModule, CommonModule],
-  declarations: [SystemComponent, MainPageComponent],
+  declarations: [SystemComponent, MainPageComponent, HeaderComponent, FooterComponent],
 })
 export class SystemModule {}
