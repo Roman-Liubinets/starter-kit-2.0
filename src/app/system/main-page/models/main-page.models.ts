689563ff9c6d4ca4ec2c0b5f1a81fc1b6c067bd2
Roman-Liubinets <roman.liubinets@gmail.com>
2019-01-31 14:04:39 +0200

    create Loaded element model and actions

---
+++ src/app/system/main-page/models/main-page.models.ts
@@ -0,0 +1,6 @@
+export interface LoadedItem {
+  _id: string;
+  first?: string;
+  last?: string;
+  something?: string;
+}
---
+++ src/app/system/main-page/store/action/main-page.actions.ts
@@ -0,0 +1,23 @@
+import { Action } from '@ngrx/store';
+import * as fromMPModels from '../../models/main-page.models';
+
+export const LOAD_ITEM = '[MP] Load item';
+export const LOAD_ITEM_SUCCESS = '[MP] Load item Success';
+export const LOAD_ITEM_FAIL = '[MP] Load item Fail';
+
+export class LoadItem implements Action {
+  readonly type = LOAD_ITEM;
+  constructor(public payload: fromMPModels.LoadedItem) {}
+}
+
+export class LoadItemSuccess implements Action {
+  readonly type = LOAD_ITEM_SUCCESS;
+  constructor(public payload: fromMPModels.LoadedItem) {}
+}
+
+export class LoadItemFail implements Action {
+  readonly type = LOAD_ITEM_FAIL;
+  constructor(public payload: fromMPModels.LoadedItem) {}
+}
+
+export type MainPageActions = LoadItem | LoadItemSuccess | LoadItemFail;
